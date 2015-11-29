fd0e81e806c70deae88a990f3a1b8718a81a142b
Luis Crespo <luiscrisf@gmail.com>
2015-11-29 15:04:05 -0430

    Defines a prop on ListContainer to initialize items.

--- src/components/ListContainer.jsx
+++ src/components/ListContainer.jsx
@@ -1,11 +1,16 @@
 import AltContainer from 'alt/AltContainer';
 import React from 'react';
 
+import ListActions from '../actions/ListActions';
 import ListStore from '../stores/ListStore';
 
 import List from './List';
 
 class ListContainer extends React.Component {
+  componentDidMount() {
+    ListActions.updateItems(this.props.initialItems)
+  }
+
   render() {
     return (
       <AltContainer store={ListStore}>
@@ -14,5 +19,13 @@ class ListContainer extends React.Component {
     );
   }
 }
+
+ListContainer.propTypes = {
+  initialItems: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string
+    })
+  )
+};
 
 export default ListContainer;
