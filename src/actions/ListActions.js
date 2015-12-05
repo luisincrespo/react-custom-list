b81bed81ad4c2ddc0c44d8d8a02bf9a4fb015fab
Luis Crespo <luiscrisf@gmail.com>
2015-12-05 14:59:35 -0430

    Adds action to edit list item.

--- src/actions/ListActions.js
+++ src/actions/ListActions.js
@@ -12,6 +12,10 @@ class ListActions {
   removeItem(index) {
     this.dispatch(index)
   }
+
+  editItem(index, item) {
+    this.dispatch({index, item})
+  }
 }
 
 export default Alt.createActions(ListActions);
