12209fe6f90f97bd40eb210070f569b0a6d60066
Luis Crespo <luiscrisf@gmail.com>
2015-12-05 16:38:20 -0430

    Binds search item action to store.

--- src/stores/ListStore.js
+++ src/stores/ListStore.js
@@ -6,29 +6,41 @@ import ListActions from '../actions/ListActions';
 class ListStore {
   constructor() {
     this.items = Immutable.List();
+    this.auxItems = Immutable.List();
 
     this.bindListeners({
       handleUpdateItems: ListActions.UPDATE_ITEMS,
       handleAddItem: ListActions.ADD_ITEM,
       handleRemoveItem: ListActions.REMOVE_ITEM,
-      handleEditItem: ListActions.EDIT_ITEM
+      handleEditItem: ListActions.EDIT_ITEM,
+      handleSearchItem: ListActions.SEARCH_ITEM
     });
   }
 
   handleUpdateItems(items) {
-    this.items = Immutable.fromJS(items)
+    this.items = Immutable.fromJS(items);
+    this.auxItems = Immutable.fromJS(items);
   }
 
   handleAddItem(item) {
-    this.items = this.items.push(Immutable.fromJS(item))
+    this.items = this.items.push(Immutable.fromJS(item));
+    this.auxItems = this.auxItems.push(Immutable.fromJS(item));
   }
 
   handleRemoveItem(index) {
-    this.items = this.items.delete(index)
+    this.items = this.items.delete(index);
+    this.auxItems = this.auxItems.delete(index);
   }
 
   handleEditItem({index, item}) {
-    this.items = this.items.set(index, Immutable.fromJS(item))
+    this.items = this.items.set(index, Immutable.fromJS(item));
+    this.auxItems = this.auxItems.delete(index);
+  }
+
+  handleSearchItem(query) {
+    this.items = this.auxItems.filter(
+      (item) => item.get('name').toLowerCase().startsWith(query.toLowerCase())
+    );
   }
 }
 
